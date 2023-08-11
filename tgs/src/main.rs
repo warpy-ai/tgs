e5fe6acaa5816c5a2a5dcd9904582b85afb7ef69
jucasoliveira <jucas.oliveira@gmail.com>
2023-08-11 08:27:46 +0100

    feat(main.rs): add reverse_string function to reverse a given string
    test(main.rs): add unit tests for reverse_string function

--- tgs/src/main.rs
+++ tgs/src/main.rs
@@ -7,3 +7,19 @@ fn main() {
         println!("You entered: {}", input.trim());
     }
 }
+
+pub fn reverse_string(s: &str) -> String {
+    s.chars().rev().collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_reverse_string() {
+        assert_eq!(reverse_string("tgs"), "sgt");
+        assert_eq!(reverse_string("hello"), "olleh");
+        assert_eq!(reverse_string(""), "");
+    }
+}
