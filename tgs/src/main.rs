8e836e9a2e21ed009c9b9d1047953c067e83fbc4
jucasoliveira <jucas.oliveira@gmail.com>
2023-08-11 08:10:29 +0100

    Initial commit: Basic Rust project setup for TGS.

--- tgs/src/main.rs
+++ tgs/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    loop {
+        print!("tgs> ");
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).unwrap();
+        // TODO: Process input
+        println!("You entered: {}", input.trim());
+    }
 }
