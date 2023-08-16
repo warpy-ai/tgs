bf3fe2d56199716f1419b981639011f4ee2dab27
jucasoliveira <jucas.oliveira@gmail.com>
2023-08-16 21:26:38 +0100

    fix(shell): remove unused import of std::process::Command, ExitStatus refactor(shell): refactor execute function to use command.output() instead of command.spawn() and child.wait() fix(tgs_handler): remove unused import of std::io::Error, ErrorKind refactor(tgs_handler): change search function to return Result<PathBuf, Error> instead of Result<(), Error> refactor(tgs_handler): remove unnecessary panic! when encountering symlink refactor(tgs_handler): change find_binary function to return Result<PathBuf, Error> instead of Result<PathBuf, std::io::Error> refactor(main): add debug print statement to print the value of bin variable

--- tgs/crates/shell/src/lib.rs
+++ tgs/crates/shell/src/lib.rs
@@ -1,5 +1,5 @@
 use std::io;
-use std::process::{Command, ExitStatus};
+use std::process::ExitStatus;
 
 #[cfg(unix)]
 use std::os::unix::process::ExitStatusExt;
@@ -7,17 +7,30 @@ use std::os::unix::process::ExitStatusExt;
 pub fn execute(bin: &str, args: &[&str]) -> Result<ExitStatus, io::Error> {
     // Handle built-in commands
     match bin {
-        "cd" => {
-            let new_dir = args.get(0).map_or("/", |x| *x);
-            std::env::set_current_dir(new_dir)?;
-            Ok(ExitStatus::from_raw(0)) // Return a successful exit status
-        }
         _ => {
             // Execute the command
-            let mut command = Command::new(bin);
-            command.args(args);
-            let mut child = command.spawn()?;
-            child.wait()
+            let mut command = {
+                let mut command = ::std::process::Command::new(bin);
+                command.args(args);
+                command
+            };
+
+            match command.output() {
+                Ok(output) => {
+                    let exit_code =
+                        output
+                            .status
+                            .code()
+                            .unwrap_or(if output.status.success() { 0 } else { 1 });
+                    println!("{}", String::from_utf8_lossy(&output.stdout[..]));
+                    eprintln!("{}", String::from_utf8_lossy(&output.stderr[..]));
+                    Ok(ExitStatus::from_raw(exit_code))
+                }
+                Err(e) => {
+                    eprintln!("{}", e);
+                    Ok(ExitStatus::from_raw(126))
+                }
+            }
         }
     }
 }
