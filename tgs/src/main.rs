b7689ce9c50d4c985ab316072b13037b5e0002e8
jucasoliveira <jucas.oliveira@gmail.com>
2023-09-01 18:36:48 +0100

    chore(Cargo.toml): remove tgs_bridge dependency from Cargo.toml
    refactor(main.rs): remove tgs_bridge module import and related code
    
    The tgs_bridge dependency was removed from the Cargo.toml file as it is no longer needed. In the main.rs file, the tgs_bridge module import and the code related to interpreting commands using tgs_bridge were removed. This is because the functionality provided by tgs_bridge is no longer required in the application.

--- tgs/src/main.rs
+++ tgs/src/main.rs
@@ -1,5 +1,4 @@
 use std::io::{self, Write};
-use tgs_bridge;
 use tgs_handler;
 use tgs_shell;
 fn main() {
@@ -13,22 +12,6 @@ fn main() {
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
         let input = input.trim(); // Trim whitespace
-
-        match tgs_bridge::interpret_command(input) {
-            Ok(interpreted_command) => {
-                // Use interpreted_command for further processing
-                println!("Interpreted Command: {}", interpreted_command);
-            }
-            Err(e) => {
-                eprintln!("NLP Interpretation Error: {}", e);
-                continue; // Skip the rest of the loop iteration
-            }
-        }
-
-        // 3. Exit the shell if the user types "exit".
-        if input == "exit" {
-            break;
-        }
 
         // 4. Execute the command using tgs_handler.
         let value: Vec<&str> = input.split(" ").collect();
