e49dcf9b264cbbc6df3def1a1856786479e0465b
jucasoliveira <jucas.oliveira@gmail.com>
2023-09-27 09:13:20 +0100

    Fix tgs_t5_finetunned model

--- tgs/crates/tgs_t5_finetunned/src/lib.rs
+++ tgs/crates/tgs_t5_finetunned/src/lib.rs
@@ -1,33 +1,89 @@
+// Necessary imports
+use anyhow::Result;
 use rust_bert::resources::{LocalResource, ResourceProvider};
 use rust_bert::t5::{T5Config, T5ForConditionalGeneration};
-use rust_tokenizers::tokenizer::T5Tokenizer;
+use rust_bert::Config;
+use rust_tokenizers::tokenizer::{T5Tokenizer, Tokenizer, TruncationStrategy};
 use std::path::PathBuf;
 use tch::{nn, Device};
 
-fn main() -> anyhow::Result<()> {
+fn return_command(input_text: &str) -> Result<String> {
+    // Define the paths for the model's configuration, tokenizer, and weights
+    println!("Loading model...");
     let config_resource = LocalResource {
-        local_path: PathBuf::from("tgs/crates/tgs_t5_finetunned/model/config.json"),
+        local_path: PathBuf::from("model/config.json"),
     };
     let sentence_piece_resource = LocalResource {
-        local_path: PathBuf::from("tgs/crates/tgs_t5_finetunned/model/spiece.model"),
+        local_path: PathBuf::from("model/spiece.model"),
     };
     let weights_resource = LocalResource {
-        local_path: PathBuf::from("tgs/crates/tgs_t5_finetunned/model/pytorch_model.bin"),
+        local_path: PathBuf::from("model/pytorch_model.bin"),
     };
 
+    let config_path = config_resource.get_local_path()?;
+    let spiece_path = sentence_piece_resource.get_local_path()?;
+    let weights_path = weights_resource.get_local_path()?;
+
+    // Check for available CUDA device
     let device = Device::cuda_if_available();
     let mut vs = nn::VarStore::new(device);
 
-    let tokenizer = T5Tokenizer::from_file(
-        sentence_piece_resource.get_local_path()?.to_str().unwrap(),
-        true,
-    )?;
-    let config = T5Config::from_file(config_resource.get_local_path()?);
+    let config = T5Config::from_file(config_path);
     let t5_model = T5ForConditionalGeneration::new(&vs.root(), &config);
+    let tokenizer = T5Tokenizer::from_file(spiece_path.to_str().unwrap(), false)?;
+    vs.load(weights_path)?;
 
-    vs.load(weights_resource.get_local_path()?)?;
+    // Mock input for now
+    let tokens = tokenizer.encode(input_text, None, 512, &TruncationStrategy::DoNotTruncate, 0);
 
-    // Your code for inference here
+    // Convert TokenizedInput to Tensor
+    let input_tensor = tokens.token_ids.as_slice().into();
 
-    Ok(())
+    println!("Tokens: {}", input_tensor);
+
+    // Inference using forward_t with all required arguments
+    let model_output = t5_model.forward_t(
+        Some(&input_tensor),
+        None,  // attention_mask
+        None,  // encoder_output
+        None,  // decoder_input
+        None,  // position_ids
+        None,  // encoder_attention_mask
+        None,  // past_key_values
+        None,  // use_cache
+        false, // train
+    );
+
+    // Extract the logits from the model output
+    let output = model_output.decoder_output;
+
+    // Convert the Tensor to a Vec<i64>
+    let output_vec: Vec<i64> = output.iter::<i64>().unwrap().collect();
+
+    // Decode the predictions
+    let decoded_output = tokenizer.decode(&output_vec[..], true, true);
+
+    // Print the decoded output
+    println!("Output: {}", decoded_output);
+
+    Ok(decoded_output)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_main() -> Result<()> {
+        // Call the main function
+        let result = return_command("list all files in a directory");
+
+        // Print the result for inspection
+        println!("Translation result: {:?}", result);
+
+        // Assert specific conditions on the result
+        // For example, ensure the result is not empty
+
+        Ok(())
+    }
 }
