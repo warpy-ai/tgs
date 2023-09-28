a10302ef6f0ad1de06725901182a08920b0c1d4c
jucasoliveira <jucas.oliveira@gmail.com>
2023-09-28 08:11:02 +0100

    fix(lib.rs): add support for 'exit' command with a special exit code of 200 feat(config.json): add new configuration options for dense_act_fn, feed_forward_proj, is_gated_act, num_decoder_layers, and relative_attention_max_distance feat(pytorch_model.bin): update model weights feat(spiece.model): add new sentence piece model file feat(lib.rs): implement function return_command to load model, tokenize input, run inference, and return decoded output feat(main.rs): add main function to call return_command and print translation result feat(main.rs): add support for inferring commands using tgs_t5_finetunned if command is not found in tgs_handler feat(main.rs): handle inferred command by finding binary path and executing it with arguments

--- tgs/crates/tgs_t5_finetunned/src/lib.rs
+++ tgs/crates/tgs_t5_finetunned/src/lib.rs
@@ -4,44 +4,55 @@ use rust_bert::resources::{LocalResource, ResourceProvider};
 use rust_bert::t5::{T5Config, T5ForConditionalGeneration};
 use rust_bert::Config;
 use rust_tokenizers::tokenizer::{T5Tokenizer, Tokenizer, TruncationStrategy};
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use tch::{nn, Device};
 
-fn return_command(input_text: &str) -> Result<String> {
+pub fn return_command(input_text: &str) -> Result<String> {
     // Define the paths for the model's configuration, tokenizer, and weights
-    println!("Loading model...");
+    println!("1. Loading model...");
+    let root_path = Path::new(env!("CARGO_MANIFEST_DIR"));
+
+    let config_path = root_path.join("model/config.json");
     let config_resource = LocalResource {
-        local_path: PathBuf::from("model/config.json"),
-    };
-    let sentence_piece_resource = LocalResource {
-        local_path: PathBuf::from("model/spiece.model"),
-    };
-    let weights_resource = LocalResource {
-        local_path: PathBuf::from("model/pytorch_model.bin"),
+        local_path: PathBuf::from(config_path),
     };
 
+    let spiece_path = root_path.join("model/spiece.model");
+    let sentence_piece_resource = LocalResource {
+        local_path: PathBuf::from(spiece_path),
+    };
+
+    let weights_path = root_path.join("model/pytorch_model.bin");
+    let weights_resource = LocalResource {
+        local_path: PathBuf::from(weights_path),
+    };
+
+    println!("2. Getting local paths...");
     let config_path = config_resource.get_local_path()?;
     let spiece_path = sentence_piece_resource.get_local_path()?;
     let weights_path = weights_resource.get_local_path()?;
 
+    println!("3. Initializing device and var store...");
     // Check for available CUDA device
     let device = Device::cuda_if_available();
     let mut vs = nn::VarStore::new(device);
 
+    println!("4. Loading model configuration and tokenizer...");
     let config = T5Config::from_file(config_path);
     let t5_model = T5ForConditionalGeneration::new(&vs.root(), &config);
     let tokenizer = T5Tokenizer::from_file(spiece_path.to_str().unwrap(), false)?;
     vs.load(weights_path)?;
 
     // Mock input for now
+    println!("5. Tokenizing input...");
     let tokens = tokenizer.encode(input_text, None, 512, &TruncationStrategy::DoNotTruncate, 0);
 
     // Convert TokenizedInput to Tensor
     let input_tensor = tokens.token_ids.as_slice().into();
-
-    println!("Tokens: {}", input_tensor);
+    println!("6. Tokens: {}", input_tensor);
 
     // Inference using forward_t with all required arguments
+    println!("7. Running inference...");
     let model_output = t5_model.forward_t(
         Some(&input_tensor),
         None,  // attention_mask
@@ -57,14 +68,16 @@ fn return_command(input_text: &str) -> Result<String> {
     // Extract the logits from the model output
     let output = model_output.decoder_output;
 
+    println!("8. Converting tensor to Vec<i64>...");
     // Convert the Tensor to a Vec<i64>
     let output_vec: Vec<i64> = output.iter::<i64>().unwrap().collect();
 
     // Decode the predictions
+    println!("9. Decoding predictions...");
     let decoded_output = tokenizer.decode(&output_vec[..], true, true);
 
     // Print the decoded output
-    println!("Output: {}", decoded_output);
+    println!("10. Output: {}", decoded_output);
 
     Ok(decoded_output)
 }
