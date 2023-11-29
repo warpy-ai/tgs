cd45bb207fb1e27493954849cc7e8ef71bd72a6b
jucasoliveira <jucas.oliveira@gmail.com>
2023-11-29 06:40:01 +0000

    add changes

--- tgs/crates/tgs_t5_finetunned/src/main.rs
+++ tgs/crates/tgs_t5_finetunned/src/main.rs
@@ -3,7 +3,7 @@ use tgs_t5_finetunned::return_command;
 fn main() {
     // TODO: Use lib function return_command and check returned value
     println!("Command: {}", "Prints sorted list of logged in users.");
-    let result = return_command("Prints sorted list of logged in users.");
+    let result = return_command("list all files in this directory");
 
     println!("Translation result: {:?}", result);
 }
